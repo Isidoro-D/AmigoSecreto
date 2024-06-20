1ce5d0f0474b435e531887394e27cbb2551340fb
Isidoro <marcelo.idantonio@senacsp.edu.br>
2024-06-19 22:16:06 -0300

    criação do comando de sortear, falta separar

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ const srt = document.querySelector(".srt");
 const rst = document.querySelector(".rst");
 const username = document.querySelector(".name");
 const listBox = document.querySelector(".name-box");
+const list = document.querySelector(".names");
 let participantes = [];
 
 // BOTÃO ADICIONAR
@@ -22,6 +23,13 @@ rst.addEventListener("click", (event) => {
 
 });
 
+srt.addEventListener("click", (event) => {
+  event.preventDefault;
+
+  prizes();
+
+});
+
 function addName() {
   let usernameValue = username.value;
 
@@ -32,8 +40,8 @@ function addName() {
   } else if (listBox.textContent.length === 0) {
     listBox.innerText = username.value.toUpperCase();
 
-                  //quando você não coloca comparativo, ela entende como se estivesse verificando como "true", se eu quisesse como "false" eu colocaria no inicio um "!"
-  } else if ( participantes.includes(username.value.toUpperCase()))  {
+    //quando você não coloca comparativo, ela entende como se estivesse verificando como "true", se eu quisesse como "false" eu colocaria no inicio um "!"
+  } else if (participantes.includes(username.value.toUpperCase())) {
     errorInput(username, "Já colocaram esse nome!");
     return;
 
@@ -51,7 +59,31 @@ function addName() {
 function restart() {
   form.reset();
   listBox.innerText = '';
+  list.innerText = '';
   participantes = [];
+}
+
+// BOTÃO SORTEIO
+
+function prizes() {
+  if (participantes.length < 4) {
+    errorInput(username,'Adicione ao menos 4 participantes!');
+    return;
+  }
+
+  /* for (let i = 0; i < participantes.length; i++) {
+    if (i == participantes.length - 1) {
+      list.innerHTML = list.innerHTML + '<li>' + participantes[i] + '-->' + participantes[0] + '</li>'
+    } else {
+      list.innerHTML = list.innerHTML + '<li>' + participantes[i] + '-->' + participantes[i + 1] + '</li>'
+    }
+  } */
+  random();
+}
+
+function random() {
+  let sorteio = participantes.sort(() => Math.random() - 0.5);
+  list.innerText = sorteio;
 }
 
 
@@ -59,11 +91,7 @@ function restart() {
 
 
 
-
-
-
-
-
+// FUNÇÃO DE ERRO
 
 function errorInput(input, message) {
   const formItem = input.parentElement;
